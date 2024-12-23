d681854728f45926fc034f5b503b67bb75396d98
dziad <dziad@MSIMODERN15>
2024-12-23 23:26:18 +0100

    Dodaj pliki projektów.

---
+++ Kalendarz/Areas/Identity/Data/KalendarzDBContext.cs
@@ -0,0 +1,30 @@
+﻿using Kalendarz.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Kalendarz.Areas.Identity.Data;
+
+public class KalendarzDBContext : IdentityDbContext<KalendarzUser>
+{
+    public KalendarzDBContext(DbContextOptions<KalendarzDBContext> options)
+        : base(options)
+    {
+    }
+    private class KalendarzUserEntityConfiguration : IEntityTypeConfiguration<KalendarzUser>
+    {
+        public void Configure(EntityTypeBuilder<KalendarzUser> builder)
+        {
+            builder.Property(x => x.FirstName).HasMaxLength(255);
+            builder.Property(x => x.LastName).HasMaxLength(255);
+        }
+    }
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ Kalendarz/Areas/Identity/Data/KalendarzUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace Kalendarz.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the KalendarzUser class
+public class KalendarzUser : IdentityUser
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
